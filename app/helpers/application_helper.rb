641672b09426975be0d65bc2155953c1d0e27cd6
Calvin <calvin.jaeho.lee@gmail.com>
2018-06-07 11:28:50 -0400

    Refactored font awesome icon buttons into application helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def fa_link_helper(fa_class, text)
+		return raw('<i class="') + fa_class + raw('"></i>') +  " #{text}"
+	end
+
+	def fa_button_helper(fa_class, text, link, params={})
+		return link_to fa_link_helper(fa_class, text), link, 
+			class: params[:class].nil? ? "btn btn-default btn-lg" : params[:class], 
+			style: params[:style].nil? ? "" : params[:style],
+			target: params[:target].nil? ? "" : params[:target]
+	end
 end
